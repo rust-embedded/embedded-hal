b27423e259c2330d8d376f4b8d971010b8ea47fd
Jorge Aparicio <jorge@japaric.io>
2018-01-20 13:18:14 +0100

    digital::IoPin

--- src/digital.rs
+++ src/digital.rs
@@ -14,3 +14,29 @@ pub trait OutputPin {
     /// Sets the pin high
     fn set_high(&mut self);
 }
+
+/// A pin that can switch between input and output modes at runtime
+pub trait IoPin {
+    /// Signals that a method was used in the wrong mode
+    type Error;
+
+    /// Sets the pin low
+    ///
+    /// **NOTE** Automatically switches the pin to output mode
+    fn set_low(&mut self);
+    /// Sets the pin high
+    ///
+    /// **NOTE** Automatically switches the pin to output mode
+    fn set_high(&mut self);
+
+    /// Checks if the pin is being driven low
+    ///
+    /// **NOTE** Automatically switches the pin to input mode
+    /// **NOTE** Takes `&mut self` because needs to modify the configuration register
+    fn is_low(&mut self) -> bool;
+    /// Checks if the pin is being driven high
+    ///
+    /// **NOTE** Automatically switches the pin to input mode
+    /// **NOTE** Takes `&mut self` because needs to modify the configuration register
+    fn is_high(&mut self) -> bool;
+}
