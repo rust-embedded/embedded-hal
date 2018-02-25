7dd3af70fc3435c7d7c4b68334d93efc9797d3e5
Daniel Egger <daniel.egger@axiros.com>
2018-02-25 13:05:52 +0100

    Added new (yet unproven) blocking Read trait for a rngs.
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

---
+++ src/blocking/rng.rs
@@ -0,0 +1,10 @@
+//! Blocking hardware random number generator
+
+/// Blocking read
+pub trait Read {
+    /// Error type
+    type Error;
+
+    /// Reads enough bytes from hardware random number generator to fill `buffer`
+    fn read(&mut self, buffer: &mut [u8]) -> Result<(), Self::Error>;
+}
