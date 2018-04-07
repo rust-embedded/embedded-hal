97fac7697cf52968796534542aab76a95e32d433
Astro <astro@spaceboyz.net>
2018-04-08 00:11:07 +0200

    Remove reading functions from OutputPin trait

--- src/digital.rs
+++ src/digital.rs
@@ -2,12 +2,6 @@
 
 /// Single digital output pin
 pub trait OutputPin {
-    /// Is the output pin high?
-    fn is_high(&self) -> bool;
-
-    /// Is the output pin low?
-    fn is_low(&self) -> bool;
-
     /// Sets the pin low
     fn set_low(&mut self);
 
