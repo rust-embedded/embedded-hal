3f61ef25d9244fcf37226f1b7948f08e5eca89b9
Astro <astro@spaceboyz.net>
2018-04-08 01:17:31 +0200

    Add StatefulOutputPin to read and toggle the state of OutputPin

--- src/digital.rs
+++ src/digital.rs
@@ -9,6 +9,25 @@ pub trait OutputPin {
     fn set_high(&mut self);
 }
 
+/// Output pin that can read its output state
+#[cfg(feature = "unproven")]
+trait StatefulOutputPin: OutputPin {
+    /// Is the pin set to high?
+    fn is_set_high(&self) -> bool;
+
+    /// Is the pin set to low?
+    fn is_set_low(&self) -> bool;
+
+    /// Toggle pin output
+    fn toggle(&mut self) {
+        if self.is_set_low() {
+            self.set_high();
+        } else {
+            self.set_low();
+        }
+    }
+}
+
 /// Single digital input pin
 #[cfg(feature = "unproven")]
 pub trait InputPin {
