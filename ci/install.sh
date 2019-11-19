df45f83ad3021688f86c7fe7f09dd3baed1b8a9c
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2019-11-19 22:40:36 +0000

    Fixup CI.

--- ci/install.sh
+++ ci/install.sh
@@ -4,6 +4,7 @@ main() {
     if [ $TARGET != x86_64-unknown-linux-gnu ]; then
         rustup target add $TARGET
     fi
+    rustup component add rustfmt
 }
 
 main
--- ci/script.sh
+++ ci/script.sh
@@ -2,11 +2,8 @@ set -euxo pipefail
 
 main() {
     cargo check --target $TARGET
-    cargo check --target $TARGET --features unproven
-
-    if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        cargo test --target $TARGET --features unproven
-    fi
+    cargo test --target $TARGET
+	cargo fmt -- --check
 }
 
 main
