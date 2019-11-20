64d2cfb23beacaae5ddc9d09fe6879fc23d5bcb3
Boris Vinogradov <no111u3@gmail.com>
2019-11-20 12:12:10 +0300

    Automate code formatting check on CI

--- ci/script.sh
+++ ci/script.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 main() {
     cargo check --target $TARGET
     cargo check --target $TARGET --features unproven
+    cargo fmt -- --check
 
     if [ $TRAVIS_RUST_VERSION = nightly ]; then
         cargo test --target $TARGET --features unproven
