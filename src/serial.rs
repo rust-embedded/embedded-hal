a1241fb4978e493fa4bf1a1c556403454dc5d6ba
Unizippro <madas@astrupa.dk>
2020-05-13 11:21:48 +0200

    Proposal for Configure baudrate trait

--- src/serial.rs
+++ src/serial.rs
@@ -25,3 +25,13 @@ pub trait Write<Word> {
     /// Ensures that none of the previously written words are still buffered
     fn try_flush(&mut self) -> nb::Result<(), Self::Error>;
 }
+
+/// Enable changing the baudrate after initiation of serial interface
+pub trait ConfigureBaud {
+    /// Baudrate type
+    type BaudRate;
+    /// Set baud error
+    type Error;
+    /// Change baudrate
+    fn set_baudrate(&mut self, baudrate: Self::BaudRate) -> Result<(), Self::Error>;
+}
