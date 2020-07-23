3e13ccfec103aba319d18366a614b65734be4dde
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-23 16:00:10 +0200

    some docstrings

--- src/blocking/dac.rs
+++ src/blocking/dac.rs
@@ -1,6 +1,10 @@
-//! Trait for digital to analog conversion
+//! Blocking DAC trait for single channel digital to analog conversion
 
-/// Represents a single DAC channel.
+/// A single DAC channel. Word is the type used to represent a single sample, this would typically
+/// be u8, u16 or u32.
+/// Note that not all bits will always be used. A 12 bit DAC for example will probably use u16 here
+/// <DISCUSSION> should we prescribe to use the most significant bits here for compat between word
+///   sizes?
 pub trait DAC<WORD> {
     /// Error type returned by DAC methods
     type Error;
