c28d0ab830e7e20a9515308018b9fa2f3c4719c0
Diego Barrios Romero <eldruin@gmail.com>
2021-03-11 10:09:16 +0100

    Fix code formatting

--- src/pwm.rs
+++ src/pwm.rs
@@ -88,8 +88,11 @@ pub trait Pwm {
     fn try_get_max_duty(&self) -> Result<Self::Duty, Self::Error>;
 
     /// Sets a new duty cycle
-    fn try_set_duty(&mut self, channel: &Self::Channel, duty: Self::Duty)
-        -> Result<(), Self::Error>;
+    fn try_set_duty(
+        &mut self,
+        channel: &Self::Channel,
+        duty: Self::Duty,
+    ) -> Result<(), Self::Error>;
 
     /// Sets a new PWM period
     fn try_set_period<P>(&mut self, period: P) -> Result<(), Self::Error>
