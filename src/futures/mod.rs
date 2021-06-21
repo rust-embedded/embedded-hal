ff085facf01cd07e5b8e8069c8e6a6fcf15860cb
Lachlan Sneff <lachlan.sneff@gmail.com>
2021-06-21 17:28:39 -0400

    Fix some of the docs

--- src/futures/mod.rs
+++ src/futures/mod.rs
@@ -1,20 +1,7 @@
-//! Non-blocking API
+//! Asynchronous APIs
 //!
-//! These traits make use of the [`nb`] crate
-//! (*please go read that crate documentation before continuing*) to abstract over
-//! the execution model and to also provide an optional blocking operation mode.
-//!
-//! The `nb::Result` enum is used to add an [`Error::WouldBlock`] variant to the errors
-//! of the traits. Using this it is possible to execute actions in a non-blocking
-//! way.
-//!
-//! `block!`, `Result` and `Error` from the [`nb`] crate are re-exported here to avoid
-//! crate version mismatches. These should be used instead of importing the `nb` crate
-//! directly again in dependent crates.
-//!
-//! [`nb`]: https://crates.io/crates/nb
+//! This traits use `core::future::Future` and generic associated types.
 
-pub use nb::{block, Error, Result};
 pub mod adc;
 pub mod capture;
 pub mod rng;
