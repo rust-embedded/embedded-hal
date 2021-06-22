1928f77b4155293e3d5c828e63176e3082e2174e
Lachlan Sneff <lachlan.sneff@gmail.com>
2021-06-22 15:16:17 -0400

    Make futures::spi::{Read, Write, ReadWrite, ReadWriteInPlace} traits

--- src/futures/spi.rs
+++ src/futures/spi.rs
@@ -1,19 +1,35 @@
 //! Serial Peripheral Interface
 
-use core::future::Future;
+use core::{future::Future, mem::MaybeUninit};
 
-/// Async transfer
-pub trait Transfer<Word: 'static> {
+/// Async read + write
+pub trait ReadWrite<Word: 'static> {
     /// Error type
     type Error;
 
     /// Associated future for the `transfer` method.
-    type TransferFuture<'a>: Future<Output = Result<&'a [Word], Self::Error>> + 'a
+    type ReadWriteFuture<'a>: Future<Output = Result<(), Self::Error>> + 'a
     where
         Self: 'a;
 
-    /// Writes `words` to the slave. Returns the `words` received from the slave
-    fn transfer<'a>(&'a mut self, words: &'a mut [Word]) -> Self::TransferFuture<'a>;
+    /// Writes `words` to the slave from the `write` buffer. Puts the words returned in the `read` buffer.
+    /// This method uses separate `write` and `read` buffers.
+    fn readwrite<'a>(&'a mut self, write: &'a [Word], read: &'a mut [MaybeUninit<Word>]) -> Self::ReadWriteFuture<'a>;
+}
+
+/// Async read + write in place.
+pub trait ReadWriteInPlace<Word: 'static> {
+    /// Error type
+    type Error;
+
+    /// Associated future for the `transfer` method.
+    type ReadWriteInPlaceFuture<'a>: Future<Output = Result<(), Self::Error>> + 'a
+    where
+        Self: 'a;
+
+    /// Writes `words` to the slave from the `readwrite` buffer and reads words into the same buffer.
+    /// This method uses a single `readwrite` buffer.
+    fn readwrite_inplace<'a>(&'a mut self, readwrite: &'a mut [MaybeUninit<Word>]) -> Self::ReadWriteInPlaceFuture<'a>;
 }
 
 /// Async write
@@ -28,6 +44,22 @@ pub trait Write<W> {
 
     /// Writes `words` to the slave, ignoring all the incoming words
     fn write<'a>(&'a mut self, words: &'a [W]) -> Self::WriteFuture<'a>;
+}
+
+/// Async read
+pub trait Read<W> {
+    /// Error type
+    type Error;
+
+    /// Associated future for the `read` method.
+    type ReadFuture<'a>: Future<Output = Result<(), Self::Error>> + 'a
+    where
+        Self: 'a;
+
+    /// Reads words from the slave without specifying any data to write.
+    /// The SPI hardware will send data, though what data it sends is not defined
+    /// by this trait. Some hardware can configure what values (e.g. all zeroes, all ones), some cannot.
+    fn read<'a>(&'a mut self, words: &'a mut [MaybeUninit<W>]) -> Self::ReadFuture<'a>;
 }
 
 // /// Operation for transactional SPI trait
