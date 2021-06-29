306b18795922c5ab809c6da99825b8ae71084f50
Lachlan Sneff <lachlan.sneff@gmail.com>
2021-06-29 15:09:44 -0400

    Remove MaybeUninit from futures::spi

--- src/futures/spi.rs
+++ src/futures/spi.rs
@@ -1,9 +1,9 @@
 //! Serial Peripheral Interface
 
-use core::{future::Future, mem::MaybeUninit};
+use core::future::Future;
 
 /// Async transfer
-pub trait Transfer<Word: 'static> {
+pub trait Transfer<W: 'static> {
     /// Error type
     type Error;
 
@@ -14,11 +14,11 @@ pub trait Transfer<Word: 'static> {
 
     /// Writes `words` to the slave from the `write` buffer. Puts the words returned in the `read` buffer.
     /// This method uses separate `write` and `read` buffers.
-    fn transfer<'a>(&'a mut self, write: &'a [Word], read: &'a mut [MaybeUninit<Word>]) -> Self::TransferFuture<'a>;
+    fn transfer<'a>(&'a mut self, write: &'a [W], read: &'a mut [W]) -> Self::TransferFuture<'a>;
 }
 
 /// Async transfer in place.
-pub trait TransferInPlace<Word: 'static> {
+pub trait TransferInPlace<W: 'static> {
     /// Error type
     type Error;
 
@@ -31,11 +31,11 @@ pub trait TransferInPlace<Word: 'static> {
     /// This method uses a single `readwrite` buffer.
     ///
     /// The returned buffer is the initialized `readwrite` buffer.
-    fn transfer_inplace<'a>(&'a mut self, readwrite: &'a mut [Word]) -> Self::TransferInPlaceFuture<'a>;
+    fn transfer_inplace<'a>(&'a mut self, readwrite: &'a mut [W]) -> Self::TransferInPlaceFuture<'a>;
 }
 
 /// Async write
-pub trait Write<Word> {
+pub trait Write<W> {
     /// Error type
     type Error;
 
@@ -45,11 +45,11 @@ pub trait Write<Word> {
         Self: 'a;
 
     /// Writes `words` to the slave, ignoring all the incoming words
-    fn write<'a>(&'a mut self, words: &'a [Word]) -> Self::WriteFuture<'a>;
+    fn write<'a>(&'a mut self, words: &'a [W]) -> Self::WriteFuture<'a>;
 }
 
 /// Async read
-pub trait Read<Word: 'static> {
+pub trait Read<W: 'static> {
     /// Error type
     type Error;
 
@@ -63,5 +63,5 @@ pub trait Read<Word: 'static> {
     /// by this trait. Some hardware can configure what values (e.g. 0x00, 0xFF), some cannot.
     ///
     /// The returned buffer is the initialized `words` buffer.
-    fn read<'a>(&'a mut self, words: &'a mut [MaybeUninit<Word>]) -> Self::ReadFuture<'a>;
+    fn read<'a>(&'a mut self, words: &'a mut [W]) -> Self::ReadFuture<'a>;
 }
