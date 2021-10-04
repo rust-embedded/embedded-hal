5f8ed23f20bf1670c6612666a35f8d32684273a3
Timo Kröger <timokroeger93@gmail.com>
2021-10-04 20:32:02 +0200

    can: Rename blocking methods
    The blocking traits borrowed the naming convention from socketcan.
    Rename the methods to `transmit()` and `receive()` to make them
    consistent with the `nb` traits.

--- src/can/blocking.rs
+++ src/can/blocking.rs
@@ -10,8 +10,8 @@ pub trait Can {
 
     /// Puts a frame in the transmit buffer. Blocks until space is available in
     /// the transmit buffer.
-    fn write(&mut self, frame: &Self::Frame) -> Result<(), Self::Error>;
+    fn transmit(&mut self, frame: &Self::Frame) -> Result<(), Self::Error>;
 
     /// Blocks until a frame was received or an error occured.
-    fn read(&mut self) -> Result<Self::Frame, Self::Error>;
+    fn receive(&mut self) -> Result<Self::Frame, Self::Error>;
 }
