db179e98faa91e1a4df7c84cf519cd741d4630f7
Markus Reiter <me@reitermark.us>
2024-01-17 02:02:57 +0100

    Merge `i2c` re-exports.

--- embedded-hal-async/src/i2c.rs
+++ embedded-hal-async/src/i2c.rs
@@ -16,9 +16,9 @@
 //! Since 7-bit addressing is the mode of the majority of I2C devices,
 //! `SevenBitAddress` has been set as default mode and thus can be omitted if desired.
 
-pub use embedded_hal::i2c::Operation;
 pub use embedded_hal::i2c::{
-    AddressMode, Error, ErrorKind, ErrorType, NoAcknowledgeSource, SevenBitAddress, TenBitAddress,
+    AddressMode, Error, ErrorKind, ErrorType, NoAcknowledgeSource, Operation, SevenBitAddress,
+    TenBitAddress,
 };
 
 /// Async I2c.
