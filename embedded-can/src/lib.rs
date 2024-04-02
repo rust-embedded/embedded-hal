f772c8aa9dc5e0ccc87bf5ae51feb5c118dda1a9
Liam Kinne <liam@kinne.io>
2024-04-03 00:20:02 +1000

    fix ci warning

--- embedded-can/src/lib.rs
+++ embedded-can/src/lib.rs
@@ -2,6 +2,12 @@
 
 #![warn(missing_docs)]
 #![no_std]
+// disable warning for already-stabilized features.
+// Needed to pass CI, because we deny warnings.
+// We don't immediately remove them to not immediately break older nightlies.
+// When all features are stable, we'll remove them.
+#![cfg_attr(nightly, allow(stable_features, unknown_lints))]
+#![cfg_attr(nightly, feature(async_fn_in_trait, impl_trait_projections))]
 #![allow(async_fn_in_trait)]
 
 pub mod asynch;
