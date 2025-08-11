5d35622e6c246445895c92573589300ea906d3e2
Liam Kinne <liam@kinne.io>
2025-08-11 19:00:31 +1000

    split into separate tx/rx traits

--- embedded-can/src/asynch.rs
+++ embedded-can/src/asynch.rs
@@ -1,17 +1,26 @@
 //! Async CAN API
 
-/// An async CAN interface that is able to transmit and receive frames.
-pub trait Can {
+/// An async CAN interface that is able to transmit frames.
+pub trait CanTx {
     /// Associated frame type.
     type Frame: crate::Frame;
 
     /// Associated error type.
     type Error: crate::Error;
 
-    /// Puts a frame in the transmit buffer.
-    /// Awaits until space is available in the transmit buffer.
+    /// Puts a frame in the transmit buffer or awaits until space is available
+    /// in the transmit buffer.
     async fn transmit(&mut self, frame: &Self::Frame) -> Result<(), Self::Error>;
+}
 
-    /// Awaits until a frame was received or an error occurred.
+/// An async CAN interface that is able to receive frames.
+pub trait CanRx {
+    /// Associated frame type.
+    type Frame: crate::Frame;
+
+    /// Associated error type.
+    type Error: crate::Error;
+
+    /// Awaits until a frame was received or an error occurs.
     async fn receive(&mut self) -> Result<Self::Frame, Self::Error>;
 }
