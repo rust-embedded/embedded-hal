9076b0a24d7ca5ec1fbe853f7d1cbd314b08085b
Victorien Elvinger <victorien@elvinger.fr>
2026-03-24 10:33:49 +0100

    [io] Require static lifetime bound on Error

--- embedded-io/src/lib.rs
+++ embedded-io/src/lib.rs
@@ -174,7 +174,7 @@ impl From<std::io::ErrorKind> for ErrorKind {
 ///
 /// This trait allows generic code to do limited inspecting of errors,
 /// to react differently to different kinds.
-pub trait Error: core::error::Error {
+pub trait Error: core::error::Error + 'static {
     /// Get the kind of this error.
     fn kind(&self) -> ErrorKind;
 }
